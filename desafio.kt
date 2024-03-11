a6504294962c4b0321507c55be558b5e2bef134b
Edson Ajeje <42541492+ERAjeje@users.noreply.github.com>
2024-03-11 12:02:17 -0300

    feat: Implemantadas classes do desafio

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,51 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+data class Usuario(var nome: String, var email: String, var senha: String)
 
-class Usuario
+data class Aula(var nome: String, var duracao: Int, var concluido: Boolean)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(var nome: String, var aulas: List<Aula>, var aprovado: Boolean)
 
 data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    //TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
+    val aula1 = Aula("Kotlin inicial", 12, false)
+    val aula2 = Aula("Funções em Kotlin", 15, false)
+    val aula3 = Aula("Kotlin Avançado", 12, false)
+    val desafio = Aula("Desafio de Código", 20, false)
+    
+    val aulas = mutableListOf(aula1, aula2, aula3, desafio)
+    val listaAulas: List<Aula> = aulas
+    
+    val conteudoEducacional = ConteudoEducacional("Começando com kotlin", listaAulas, false)
+    
+    val conteudos = mutableListOf(conteudoEducacional)
+    val listaConteudos: List<ConteudoEducacional> = conteudos
+    
+    val formacao = Formacao("Programação em Kotlin", listaConteudos)
+    
+    //TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val usuario1 = Usuario("Goku", "goku@coorporacaocapsula.com", "super_sayajin")
+    val usuario2 = Usuario("Sakura", "sakura@clamp.com", "aiaiai_Yukito")
+    
+    println("Número de Conteúdos Educacionais da formação: ${formacao.conteudos.size}")
+    println("Número de Aulas no Conteúdo Educacional: ${formacao.conteudos[0].aulas.size}\n")
+    
+    println("Número de inscritos inicial: ${formacao.inscritos.size}\n")
+    
+    formacao.matricular(usuario1)
+    println(formacao.inscritos[0].nome)
+    
+    formacao.matricular(usuario2)
+    println(formacao.inscritos[1].nome)
+    
+    println("\nNúmero de inscritos final: ${formacao.inscritos.size}")
 }
